1bba76cef1f84b2d6c2889e694f2912400c7c68e
tondam77-faith <tondam77@gmail.com>
2026-02-06 21:27:37 +0100

    Upgrade service worker cache version to v1.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'civics-srs-v1.0';
+const CACHE_NAME = 'civics-srs-v1.1';
 const ASSETS = [
   './',
   './index.html',
@@ -28,4 +28,5 @@ self.addEventListener('activate', (e) => {
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+
 });
